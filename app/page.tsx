594ffb51518c708dc33c05b90d1248b88b1bf00c
Lukas Lehmann <lukas.lehmann97@gmail.com>
2024-03-22 15:06:20 +0100

    change page name

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { UserSearch } from '@/app/components/UserSearch';
 import { getAllUsers } from '@/app/db/db';
 
-export default async function Home() {
+export default async function SearchPage() {
   const users = await getAllUsers();
 
   return <>
