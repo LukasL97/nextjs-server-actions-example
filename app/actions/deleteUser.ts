ae54fa0bc6893cab73a6e6ccc0b1e1be0a3a0e50
Lukas Lehmann <lukas.lehmann97@gmail.com>
2024-03-22 14:53:31 +0100

    revalidatePath

--- app/actions/deleteUser.ts
+++ app/actions/deleteUser.ts
@@ -1,7 +1,9 @@
 'use server';
 
 import { deleteUser as deleteUserFromDb } from '@/app/db/db';
+import { revalidatePath } from 'next/cache';
 
 export async function deleteUser(id: string) {
   await deleteUserFromDb(id);
+  revalidatePath('/');
 }
--- app/actions/saveUser.ts
+++ app/actions/saveUser.ts
@@ -3,10 +3,12 @@
 import { User } from '@/app/user';
 import { randomUUID } from 'crypto';
 import { putUser } from '@/app/db/db';
+import { revalidatePath } from 'next/cache';
 
 export async function saveUser(user: User) {
   if (!user.id) {
     user.id = randomUUID();
   }
   await putUser(user);
+  revalidatePath('/');
 }
