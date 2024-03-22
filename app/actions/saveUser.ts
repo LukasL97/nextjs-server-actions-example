f294dbfc29e6805ffef5b2a8e58f18adab9e0a40
Lukas Lehmann <lukas.lehmann97@gmail.com>
2024-03-22 13:41:32 +0100

    revalidatePath

--- app/actions/saveUser.ts
+++ app/actions/saveUser.ts
@@ -2,8 +2,10 @@
 
 import { User, users } from '@/app/user';
 import { randomUUID } from 'crypto';
+import { revalidatePath } from 'next/cache';
 
 export async function saveUser(user: User) {
+  console.log(`Before: ${users}`)
   if (user.id) {
     const index = users.findIndex(u => u.id === user.id);
     users[index] = user;
@@ -11,4 +13,6 @@ export async function saveUser(user: User) {
     user.id = randomUUID();
     users.push(user);
   }
+  console.log(`After: ${users}`);
+  revalidatePath('/');
 }
