acc0c26a2a69c728c48fa569331b3aea7147f71c
Lukas Lehmann <lukas.lehmann97@gmail.com>
2024-04-19 10:05:02 +0200

    add log

--- app/actions/getUsers.ts
+++ app/actions/getUsers.ts
@@ -4,6 +4,7 @@ import { User } from '@/app/user';
 import { getAllUsersFromDb } from '@/app/db';
 
 export async function getUsers(searchTerm: string): Promise<User[]> {
+  console.info(`Search users: ${searchTerm}`)
   const users = await getAllUsersFromDb();
   return users.filter(user => user.firstName.includes(searchTerm) || user.lastName.includes(searchTerm));
 }
